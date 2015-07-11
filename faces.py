f18618ce804057fe4c4a4990864be28bdc250da3
umentu <umentu@gmail.com>
2015-07-11 12:38:16 +0900

    upload faces.py
    upload faces.pu

--- faces.py
+++ faces.py
@@ -110,9 +110,9 @@ class Face(object):
         return img_opencv
 
 
-    def get_edge_faces(self, image, min_size=(100, 100), ratio=20):
+    def get_edge_faces(self, image, min_size=(100, 100)):
         """
-        顔をモザイクで覆うメソッド。
+        顔をエッジ化するメソッド。
         image: cv2.imreadで読み取った変数
         min_size: 顔判定する最小サイズの指定。
         """
@@ -142,13 +142,58 @@ class Face(object):
                                           face_width,
                                           face_height))
 
+                # OpenCV用のデータに変換
                 cut_face = np.asarray(cut_face)
 
                 # 2次微分オペレータを使って画像のエッジを検出
-                img_tmp = cv2.Laplacian(cut_face, cv2.CV_32F, 8)
+                img_tmp = cv2.Laplacian(cut_face, cv2.CV_32F, 16)
                 cut_face = cv2.convertScaleAbs(img_tmp)
 
-               # イメージをpillowで扱うことのできる形式に変換しておく。
+                # pillow用のデータに変換
+                cut_face = Image.fromarray(cut_face)
+
+                # 元の画像に加工した顔画像を貼り付ける。
+                img_edit.paste(cut_face, tuple((face_x, face_y)))
+
+
+    def get_negaposi_faces(self, image, min_size=(100, 100)):
+        """
+        顔をネガポジ変換するメソッド。
+        image: cv2.imreadで読み取った変数
+        min_size: 顔判定する最小サイズの指定。
+        """
+
+        # イメージをpillowで扱うことのできる形式に変換しておく。
+        img_edit = Image.fromarray(image)
+
+        # 顔認識
+        faces = self.get_faces(image, min_size)
+
+        # 顔があった場合。
+        if len(faces) > 0:
+
+            # 複数の顔があった場合、１つずつ四角で囲っていく。
+            for face in faces:
+
+                # 顔の部分の左上のx,y座標(微調整)
+                face_x = face[0] - 50
+                face_y = face[1] - 100
+                # 顔の部分の横の長さと縦の長さ(微調整)
+                face_width = face[0]+face[2] + 50
+                face_height = face[1]+face[3] + 100
+
+                # 顔を切り抜く
+                cut_face = img_edit.crop((face_x,
+                                          face_y,
+                                          face_width,
+                                          face_height))
+
+                # OpenCV用のデータに変換
+                cut_face = np.asarray(cut_face)
+
+                cut_face = 255 - cut_face
+
+                # pillow用のデータに変換
                 cut_face = Image.fromarray(cut_face)
 
                 # 元の画像に加工した顔画像を貼り付ける。
@@ -159,6 +204,55 @@ class Face(object):
         img_opencv = np.asarray(img_edit)
 
         return img_opencv
+
+    def get_smileman_faces(self, image, min_size=(100, 100)):
+        """
+        顔を笑い男マークにするメソッド。
+        image: cv2.imreadで読み取った変数
+        min_size: 顔判定する最小サイズの指定。
+        """
+
+        # イメージをpillowで扱うことのできる形式に変換しておく。
+        img_edit = Image.fromarray(image)
+
+        # 笑い男マーク呼び出し
+        img_smileman = Image.open("./image/smileman.jpg")
+
+        # OpenCV用のデータに変換
+        img_smileman = np.asarray(img_smileman)
+
+        # pillowではRGB、OpenCVではGBRのため、色の順番を並べ替え。
+        img_bgr = cv2.split(img_smileman)
+        img_smileman = cv2.merge((img_bgr[2], img_bgr[1], img_bgr[0]))
+
+        # pillow用のデータに変換
+        img_smileman = Image.fromarray(img_smileman)
+
+        # 顔認識
+        faces = self.get_faces(image, min_size)
+
+        # 顔があった場合。
+        if len(faces) > 0:
+
+            # 複数の顔があった場合、１つずつ四角で囲っていく。
+            for face in faces:
+
+                # 顔の部分の左上のx,y座標(微調整)
+                face_x = face[0] -50
+                face_y = face[1]
+                # 顔の部分の横の長さと縦の長さ(微調整)
+                face_height = face[1]+face[3] - 50
+
+                tmp_smileman = img_smileman.resize((face_height, face_height), Image.LINEAR)
+
+                # 元の画像に加工した顔画像を貼り付ける。
+                img_edit.paste(tmp_smileman, tuple((face_x, face_y)))
+
+        #pillow用のデータをOpenCVデータに変換
+        img_opencv = np.asarray(img_edit)
+
+        return img_opencv
+
 
 if __name__ == '__main__':
     
@@ -187,8 +281,17 @@ if __name__ == '__main__':
         # cv2.imshow('MOSAIC FACE', frame)
 
         # エッジ
-        frame = face.get_edge_faces(frame, ratio=40)
-        cv2.imshow('EDGE FACE', frame)
+        # frame = face.get_edge_faces(frame)
+        # cv2.imshow('EDGE FACE', frame)
+
+        # ネガポジ変換
+        # frame = face.get_negaposi_faces(frame)
+        # cv2.imshow("NEGAPOSI FACE", frame)
+
+        # 笑い男
+        frame = face.get_smileman_faces(frame)
+        cv2.imshow("SMILEMAN FACE", frame)
+
 
         # qを押したら終了。
         k = cv2.waitKey(1)
